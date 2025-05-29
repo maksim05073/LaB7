5e15f1d0dd9fd29d505f3d8c710bd750b77da659
maksim05073 <maksimmixcer@gmail.com>
2025-05-29 20:25:15 +0300

    Add files via upload

---
+++ LB7-main/LB7/Interfaces/IFileContainer.cs
@@ -0,0 +1,8 @@
+﻿namespace LB7;
+
+public interface IFileContainer<Device>:IContainer<Device>
+{
+    void Save( String fileName );
+    void Load( String fileName );
+    Boolean IsDataSaved {get;}
+}
---
+++ LB7-main/LB7/Program.cs
@@ -0,0 +1,12 @@
+﻿
+namespace LB7;
+
+class Program
+{
+    static void Main(string[] args)
+    
+    {
+        Device a = new Device("asd",2,new DateTime(1,11,2005));
+        Console.ReadKey();
+    }
+}
